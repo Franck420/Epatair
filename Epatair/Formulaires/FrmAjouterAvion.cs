a982cb6eabcd8a386c9d99b4c0ac4ebf65cda4ab
Franck <goku_412@hotmail.com>
2020-04-27 15:00:49 -0400

    Initial Commit

--- Epatair/Formulaires/FrmAjouterAvion.cs
+++ Epatair/Formulaires/FrmAjouterAvion.cs
@@ -16,5 +16,10 @@ namespace Epatair.Formulaires
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
