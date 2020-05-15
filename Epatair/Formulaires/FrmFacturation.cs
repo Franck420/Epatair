ccddf2b91ccdf9f897d65f8a5a0ff45f2a6ef401
stevenamki <steven-labelle@videotron.ca>
2020-05-15 11:06:22 -0400

    modifaction de FrmFacture et création de d'autre dossier(mappeur repositorie et gestion)

--- Epatair/Formulaires/FrmFacturation.cs
+++ Epatair/Formulaires/FrmFacturation.cs
@@ -16,5 +16,15 @@ namespace Epatair.Formulaires
         {
             InitializeComponent();
         }
+
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmFacturation_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
