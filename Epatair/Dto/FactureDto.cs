fc5675446853a6dd7b37b3829fd16fe680b0d660
stevenamki <steven-labelle@videotron.ca>
2020-05-15 11:33:12 -0400

    modification de FactureDto et du MappeurFacture(A revoir)

--- Epatair/Dto/FactureDto.cs
+++ Epatair/Dto/FactureDto.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Epatair.Dto
 {
-    class FactureDto
+    //Stock les donnee d une facture
+   public class FactureDto
     {
+       public int Idfacture;
+
+        public double HrVol { get; set; }
+        public double TarifHrVol { get; set; }
+        public double HrSol { get; set; }
+        public double TarifHrSol { get; set; }
+        public double TarifHrSolInstruteur { get; set; }
+        public double TarifHrVolInstruteur { get; set; }
+
+        public int IdAvion { get; set; }
+
+        public string Instructeur { get; set; }
+        public string Pilote { get; set; }
+
+        public DateTime HrDemarrage { get; set; }
+        public DateTime HrArret { get; set; }
+        public DateTime HrAtterissage { get; set; }
+        public DateTime HrDemarrage2 { get; set; }
+
     }
 }
