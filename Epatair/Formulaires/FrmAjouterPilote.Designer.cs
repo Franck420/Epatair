eafb2104713fe18b7201ed102783eae384403e2b
stevenamki <steven-labelle@videotron.ca>
2020-05-15 17:47:47 -0400

    graphie FrmajouterPilote et correction Frmmodifierpilote

--- Epatair/Formulaires/FrmAjouterPilote.Designer.cs
+++ Epatair/Formulaires/FrmAjouterPilote.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnAjouter = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmBGrade = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // BtnAjouter
+            // 
+            this.BtnAjouter.Location = new System.Drawing.Point(46, 66);
+            this.BtnAjouter.Name = "BtnAjouter";
+            this.BtnAjouter.Size = new System.Drawing.Size(100, 23);
+            this.BtnAjouter.TabIndex = 0;
+            this.BtnAjouter.Text = "Ajouter";
+            this.BtnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(199, 66);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(100, 23);
+            this.btnAnnuler.TabIndex = 1;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(5, 27);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(29, 13);
+            this.lblNom.TabIndex = 2;
+            this.lblNom.Text = "Nom";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(158, 27);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(36, 13);
+            this.lblGrade.TabIndex = 3;
+            this.lblGrade.Text = "Grade";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // cmBGrade
+            // 
+            this.cmBGrade.FormattingEnabled = true;
+            this.cmBGrade.Location = new System.Drawing.Point(200, 23);
+            this.cmBGrade.Name = "cmBGrade";
+            this.cmBGrade.Size = new System.Drawing.Size(99, 21);
+            this.cmBGrade.TabIndex = 7;
+            // 
+            // FrmAjouterPilote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(338, 111);
+            this.Controls.Add(this.cmBGrade);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.BtnAjouter);
+            this.Name = "FrmAjouterPilote";
             this.Text = "FrmAjouterPilote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnAjouter;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cmBGrade;
     }
 }
