1ac8c5013288e6dae1c62918185971640f2b7321
Franck <goku_412@hotmail.com>
2020-05-15 17:57:54 -0400

    Terminer les relations des rendez vous

---
+++ Epatair/Formulaires/FrmGestionRendezVous.cs
@@ -0,0 +1,38 @@
+﻿using Epatair.Gestion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Epatair.Formulaires
+{
+    public partial class FrmGestionRendezVous : Form
+    {
+
+        GestionRendezVous gestionRendezVous2;
+
+        public FrmGestionRendezVous(GestionRendezVous gestionRendezVous)
+        {
+            InitializeComponent();
+            gestionRendezVous2 = gestionRendezVous;
+        }
+
+     
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnModifierRDV_Click(object sender, EventArgs e)
+        {
+            Form ModifierRDV = new FrmModifierRendezVous(gestionRendezVous2);
+            ModifierRDV.Show();
+        }
+    }
+}
---
+++ Epatair/Formulaires/FrmModifierRendezVous.cs
@@ -0,0 +1,21 @@
+﻿using Epatair.Gestion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Epatair.Formulaires
+{
+    public partial class FrmModifierRendezVous : Form
+    {
+        public FrmModifierRendezVous(GestionRendezVous gestionRendezVous)
+        {
+            InitializeComponent();
+        }
+    }
+}
